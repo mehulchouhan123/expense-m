9f68975da33455dc4f7f4006db3ee3e942217304
mehulchouhan <mehul16dec@gmail.com>
2023-03-09 09:25:20 +0530

    finnaly filter is done

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,37 +1,47 @@
 class ExpensesController < ApplicationController
   def index
+    # debugger
+   
     @expense=Expense.new
     if !user_signed_in?
       redirect_to new_user_session_path
     end
-    # byebug
+
     if @current_user!=nil
-      @expenses=User.find(current_user.id).expenses
+      # @count
+      c_id=current_user.id
+
+      @expenses=Expense.where("date>=? AND user_id=?",Date.today,c_id)
+      if params[:date]!=nil
+        # debugger
+        @expenses=Expense.where("date>=? AND user_id=?",params[:date],c_id)
+      end
+      # debugger
       if params[:array]!=nil
         if params[:array].size==1
-          @expenses=Expense.where("type_of_expenses=? AND date>=",params[:array][0],params[:date])
+          @expenses=Expense.where("type_of_expenses=? AND date>=? AND user_id=?",params[:array][0],params[:date],c_id)
         elsif  params[:array].size==2
-          @expenses=Expense.where("(type_of_expenses=? OR type_of_expenses=?)AND date>=?",params[:array][0], params[:array][1].params[:date])
+          # debugger
+          @expenses=Expense.where("(type_of_expenses=? OR type_of_expenses=?)AND date>=?AND user_id=?",params[:array][0], params[:array][1],params[:date],c_id)
         elsif params[:array].size==3
-         @expenses=Expense.where("(type_of_expenses=? OR type_of_expenses=? OR  type_of_expenses=?) AND date=? ",params[:array][0],params[:array][1],params[:array][2],params[:date])
+          # debugger
+          @expenses=Expense.where("(type_of_expenses=? OR type_of_expenses=? OR  type_of_expenses=?) AND date>=? AND user_id=?",params[:array][0],params[:array][1],params[:array][2],params[:date],c_id)
+        elsif params[:array].size==4
+          @expenses=Expense.where("(type_of_expenses=? OR type_of_expenses=? OR  type_of_expenses=? OR type_of_expenses=? ) AND date>=? AND user_id=?",params[:array][0],params[:array][1],params[:array][2],params[:array][3],params[:date],c_id)
+        elsif params[:array].size==5
+          @expenses=Expense.where("(type_of_expenses=? OR type_of_expenses=? OR  type_of_expenses=? OR type_of_expenses=? OR
+            type_of_expenses=? ) AND date>=? AND user_id=?",params[:array][0],params[:array][1],params[:array][2],params[:array][3],params[:array][4],params[:date],c_id)
         end
+
       end
-
     end
-  
+    
+    # debugger
   end
-
+  
   def create
-    # byebug
-    @date=Date.today
-    if params[:date]=="Last week"
-      @date=Date.today.last_week
-    elsif params[:date]=="Last month"
-      @date=Date.today.last_month
-    elsif params[:date]=="Last year"
-      @date=Date.today.last_year
-    end
-
+    # debugger
+    
     if params[:commit]=='Save'
       cid=current_user.id
       expense=Expense.new(param_expense)
@@ -39,7 +49,16 @@ class ExpensesController < ApplicationController
       expense.save!
       redirect_to root_path
     else
-      @hash={"Education"=>0,"Clothes"=>"0","Food"=>"0"}
+      @date=Date.today
+      if params[:date]=="Last week"
+        @date=Date.today.last_week
+      elsif params[:date]=="Last month"
+        @date=Date.today.ago(1.month).beginning_of_month
+      elsif params[:date]=="Last year"
+        @date=Date.today.ago(1.year).beginning_of_year
+      end
+
+      @hash={"Education"=>"0","Food"=>"0","Housing"=>"0","Transporation"=>"0","Clothing"=>"0"}
       a=Array.new
       @hash.each do |k,v|
         v=params[k]
@@ -47,22 +66,28 @@ class ExpensesController < ApplicationController
           a.push(k)
         end
       end
-      @expenses=1
-      if a.size==1
-        @expenses=Expense.where("type_of_expenses=?",a[0])
-        puts @expenses
-      end
       redirect_to root_path(array: a,date: @date)
  
     end
   end
+
+
+
+
   def edit
     @expense=Expense.find(params[:id])
+    @date=@expense.date
 
   end
   def destroy
     @expense=Expense.find(params[:id])
     @expense.destroy
+  end
+  def update
+    @expense=Expense.find(params[:id])
+    @expense.update(param_expense)
+    redirect_to root_path
+    
   end
   private
   def param_expense
